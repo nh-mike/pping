324c9d69c32e9471961e38963568f87383e9e53e
Michael Thompson <25192401+nh-mike@users.noreply.github.com>
2021-06-16 14:12:50 +0800

    Update pping.py
    Update argument parser and add arguments for Timeout, Number of Tests (Count) and Test Forever. Provide an exit code reflecting the status of the most recent test.

--- pping.py
+++ pping.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import socket
 import sys
+import time
 
 protocols = {
     "ftp": 21,
@@ -20,26 +22,87 @@ protocols = {
 }
 
 
-def main(address, port):
-    address = validate_address(address)
-    port = validate_port(port)
+def main():
+    # Method name is too long, trying to keep my code tidy within 79 characters
+    ap = argparse.ArgumentParser
+    examples = helpExamples()
 
+    psr = ap(formatter_class=argparse.RawTextHelpFormatter,
+             description='Connect to a network port to determine if a server' +
+                         ' / service is accessible',
+             epilog=examples,
+             prefix_chars='-')
+
+    psr.add_argument('-t',
+                     action='store_true',
+                     help='Continuously attempt to connect to the port until' +
+                     ' stopped This option is incompatible with -n' + "\r\n" +
+                     'To stop, use Control + C',
+                     dest="forever")
+
+    psr.add_argument('-n',
+                     nargs=1,
+                     type=int,
+                     help='The number of times to attempt to connect to the ' +
+                     'port. This option is incompatible with -t',
+                     dest="count")
+
+    psr.add_argument('-w',
+                     nargs=1,
+                     type=int,
+                     default=1000,
+                     help='Time to wait in milliseconds to wait before ' +
+                     'trying again',
+                     dest="timeout")
+
+    psr.add_argument('address',
+                     metavar='ADDRESS',
+                     type=str,
+                     help='An IP Address or a Domain Name to target')
+
+    psr.add_argument('port',
+                     metavar='PORT',
+                     type=str,
+                     help='Port Number or a Protocol Name. See list of ' +
+                     'Supported Protocol Names below')
+
+    args = psr.parse_args()
+
+    address = validate_address(args.address)
+    port = validate_port(args.port)
+    count = validate_count(args.forever, args.count)
+    timeout = validate_timeout(args.timeout)
+
+    while count != 0:
+        try:
+            result = doPPing(address, port)
+            count -= 1
+            if count != 0:
+                time.sleep(timeout)
+        except KeyboardInterrupt:
+            break
+
+    sys.exit(result)
+
+
+def doPPing(address, port):
     a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     location = (address, int(port))
     result_of_check = a_socket.connect_ex(location)
     a_socket.close()
 
     if result_of_check == 0:
         print("Port " + str(address) + ':' + str(port) + " is open")
+        return 0
     else:
         print("Port " + str(address) + ':' + str(port) + " is not open")
+        return 1
 
 
 def is_valid_ipv4_address(address):
     try:
         socket.inet_pton(socket.AF_INET, address)
-    except AttributeError:  # no inet_pton here, sorry
+    except AttributeError:  # inet_pton doesn't exist
         try:
             socket.inet_aton(address)
         except socket.error:
@@ -94,8 +157,29 @@ def validate_port(port):
     return port
 
 
-def help():
-    selfname = os.path.basename(sys.argv[0])
+def validate_count(forever, count):
+    if forever is True and count is not None:
+        raise FatalException('Cannot use -n and -t together')
+    if forever is True:
+        return -1
+    elif count is None:
+        return 1
+    elif count[0] > 0:
+        return count[0]
+    else:
+        raise FatalException('Cannot set count to zero')
+
+
+def validate_timeout(timeout):
+    if timeout is None:
+        return 1
+    else:
+        return timeout/1000
+
+
+def helpExamples():
+    op = ''
+    selfname = '  ' + os.path.basename(sys.argv[0])
     minpad = 1
     ps = {
         'PROTOCOL': 'PORT'
@@ -110,34 +194,23 @@ def help():
 
     minpad = minpad + 2
 
-    print('"Port Ping"')
-    print('Connect to a network port to determine if a server / ' +
-          'service is accessible')
-    print('')
-    print('Usage')
-    print(selfname + ' ADDRESS PORT')
-    print('')
-    print('ADDRESS:')
-    print('For the ADDRESS option, you can supply an IP address or ' +
-          'a Domain Name')
-    print('')
-    print('PORT:')
-    print('For the PORT option, you can supply a Port Number or a ' +
-          'Protocol Name')
-    print('See list of Supported Protocol Names below')
-    print('')
-    print('EXAMPLES:')
-    print(selfname + ' 8.8.8.8 53')
-    print(selfname + ' dns.google.com dns')
-    print(selfname + ' 142.250.66.238 80')
-    print(selfname + ' google.com http')
-    print(selfname + ' 142.250.66.238 443')
-    print(selfname + ' google.com https')
-    print('')
-    print('Supported Protocol Names:')
+    op = op + 'examples:' + "\r\n"
+    op = op + selfname + ' 8.8.8.8 53' + "\r\n"
+    op = op + selfname + ' dns.google.com dns' + "\r\n"
+    op = op + selfname + ' 142.250.66.238 80' + "\r\n"
+    op = op + selfname + ' google.com http' + "\r\n"
+    op = op + selfname + ' 142.250.66.238 443' + "\r\n"
+    op = op + selfname + ' google.com https' + "\r\n"
+    op = op + "\r\n"
+    op = op + 'supported protocol names:' + "\r\n"
 
     for key in ps:
-        print(key.ljust(minpad) + str(ps[key]))
+        op = op + '  ' + str(key.ljust(minpad) + str(ps[key]) + "\r\n")
+
+    op = op + "\r\n"
+    op = op + 'This software is licensed under the LGPL-3.0 license.' + "\r\n"
+    op = op + 'https://github.com/nh-mike/pping/blob/main/LICENSE'
+    return op
 
 
 class FatalException(Exception):
@@ -145,10 +218,7 @@ class FatalException(Exception):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 3:
-        try:
-            main(sys.argv[1], sys.argv[2])
-        except FatalException as error:
-            print(repr(error))
-    else:
-        help()
+    try:
+        main()
+    except FatalException as error:
+        print(repr(error))
